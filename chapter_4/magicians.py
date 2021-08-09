c5170d6c0f46df22c79c1c6b9f0c36878fdf4273
alex-pran <apranitchii@gmail.com>
2021-08-08 21:29:39 -0400

    8/8/21

--- chapter_4/magicians.py
+++ chapter_4/magicians.py
@@ -5,3 +5,8 @@ for magician in magicians:
 
 for magician in magicians:
     print(f"\n{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.")
+
+    print("Thank you, everyone. Tha was a great magic shows!\n")
+
+print("Thank you, everyone. Tha was a great magic shows!\n")
---
+++ chapter_4/players.py
@@ -0,0 +1,15 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+
+print(players[2:])
+print(players[-3:])
+print(players[-2:])
+
+
+print("\n----------------")
+
+print("Here are first three players on my team:")
+for player in players[:3]:
+    print(player.title())
