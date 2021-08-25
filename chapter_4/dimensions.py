f89e221dda03a7d77347b8b90da484787e037837
alex-pran <82548225+alex-pran@users.noreply.github.com>
2021-08-25 16:38:26 -0400

    8/25/2021

---
+++ chapter_4/dimensions.py
@@ -0,0 +1,15 @@
+dimensions = (200, 50)
+# dimensions[0] = 250
+# print(dimensions[0])
+# print(dimensions[1])
+
+print("Original dimension:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified demensions:")
+for dimension in dimensions:
+    print(dimension)
+
+
