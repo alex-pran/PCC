59042653d0255c4f3b877e51b7349242668e3d79
alex-pran <82548225+alex-pran@users.noreply.github.com>
2021-11-23 18:59:55 -0500

    11/23/2021

---
+++ Page_92/Looping.py
@@ -0,0 +1,25 @@
+user_1 = {
+    'username': 'mascon',
+    'first': 'Alex',
+    'last': 'Pran'
+}
+for keys in sorted(user_1.keys()):
+    print(f"Keys: {keys.title()}")
+for value in sorted(user_1.keys()):
+    print(f"Value: {value}")
+
+# for key, value in user_1.items():
+#     print(f"\nKey: {key}")
+#     print(f"Value: {value}")
+
+print("------------------------------------------------")
+db = {
+    'zip': 'aabbcc',
+    'street': 'fulton',
+    'floor': 'second',
+    'number_ap': 'd2'
+}
+
+for key, value in db.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
--- Page_92/alien2.py
+++ Page_92/alien2.py
@@ -1,3 +1,5 @@
+
+# Simple dictionary
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0)
 
@@ -34,7 +36,39 @@ print("------------------------------------------------")
 new_name = cat_2['name'] = 'Tom'
 print(cat_2['name'])
 print(cat_2)
-
+print("------------------------------------------------")
 newAge = cat_1['age'] = 2
 print(cat_1['age'])
 print(cat_1)
+print("------------------------------------------------")
+cat_3 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original position {cat_3['x_position']}")
+
+if cat_3['speed'] == 'slow':
+    x_increment = 1
+
+elif cat_3['speed'] == 'medium':
+    x_increment = 2
+
+else:
+    x_increment = 3
+
+cat_3['x_position'] = cat_3['x_position'] + x_increment
+
+print(f"New x_position {cat_3['x_position']}")
+print("------------------------------------------------")
+
+# A dictionary of similar Objects
+favorite_food = {
+    'Tom': 'potato',
+    'Kate': 'watermelon',
+    'Michael': 'banana',
+    'Bobby': 'avocado'
+}
+
+print(favorite_food)
+print(favorite_food['Bobby'])
+print(favorite_food['Kate'])
+
+food = favorite_food['Tom'].title()
+print(food)
